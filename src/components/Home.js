89bd0609d557b18b5442ac9cacd2cfbe7b79cb49
firefly <lu.school2023@gmail.com>
2025-11-01 16:25:18 -0700

    feat(home): home page css styling update to home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,14 @@
 import React from "react";
+import "./Home.css"
 
 const Home = () => {
     return(
-        <div>
-            <h1>I wanna play WWM</h1>
-            <h2>or REPO</h2>
-            
-            <p>
-                I just really wanna play those games right now.
-            </p>
+        <div className="home-container">
+            <div className="content">
+                <h1>I wanna play WWM</h1>
+                <h2>or REPO</h2>
+                <p>I just really wanna play those games right now.</p>
+            </div>
         </div>
     );
 };
